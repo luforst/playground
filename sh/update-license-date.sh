35d1e46bc84fecbc879d6fb515e4ec19c3334672
Lukas Forst <lukas.forst@zoho.com>
2021-02-05 13:32:01 +0100

    Add test for whether a LICENSE file exists

--- sh/update-license-date.sh
+++ sh/update-license-date.sh
@@ -2,6 +2,12 @@
 # update-license-date.sh
 # Updates the LICENSE file's date to the current year
 
+if [ ! -s "LICENSE" ]; then # -s is test's operator for "Does the file ... exist and is it non-empty?"
+    echo No LICENSE file found!
+    echo Abort.
+    exit 1
+fi
+
 # search for line with "(c) 20xy ..."
 
 # change 20xy -> current year
