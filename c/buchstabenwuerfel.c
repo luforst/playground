2e1f0a20e29452efead3e442e67906efae49898b
Lukas Forst <lukas.forst@zoho.com>
2021-05-07 17:35:12 +0200

    Change upper limit of random number generation

--- c/buchstabenwuerfel.c
+++ c/buchstabenwuerfel.c
@@ -6,7 +6,7 @@ int main(void)
 {
         const char buchstaben[] = {"ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
         srand(time(NULL));
-        int zahl = rand() % 10;
+        int zahl = rand() % 26;
         printf("%c\n", buchstaben[zahl]);
         return 0;
 }
