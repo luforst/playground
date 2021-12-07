282336d4fb7134577937016c3b689faee37d7267
Lukas Forst <lukas.forst@zoho.com>
2021-12-07 18:57:35 +0100

    Add scanf1.c: repeats an entered integer

---
+++ c/scanf1.c
@@ -0,0 +1,11 @@
+/* scanf1.c */
+#include <stdio.h>
+
+int main(void)
+{
+	int i; /* ein ganzzahliger Datentyp */
+	printf("Zahl eingeben: ");
+	scanf("%d",&i);
+	printf("Die eingegebene Zahl war: %d\n",i);
+	return 0;
+}
