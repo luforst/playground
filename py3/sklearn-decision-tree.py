fdcfd3ad2cf75fb0990f93066cb97e432779bc77
Lukas Forst <lukas.forst@uni-ulm.de>
2025-11-11 17:11:09 +0100

    add concept for decision tree traversal

--- py3/sklearn-decision-tree.py
+++ py3/sklearn-decision-tree.py
@@ -2,10 +2,19 @@
 # Experimente mit sklearn CART trees, um Anzahl benutzter Features auszulesen
 from sklearn.tree import DecisionTreeClassifier
 from sklearn import datasets
+import pandas as pd
 
-data = datasets.load_breast_cancer()
-clf = DecisionTreeClassifier(random_state=42).fit(data[:, 0:-1],
-						  data[:, -1])
+df = pd.read_csv("Liver RNA Data.csv")
+clf = DecisionTreeClassifier(random_state=42).fit(df.iloc[0:200, 0:-1],
+						  df.iloc[0:200, -1])
 
 tree_depth = clf.tree_.max_depth
 
+def get_used_features(tree):
+    # recursive traversal of the tree
+    features = []
+    # features.append feature at root node of current subtree
+    # if there are more subtrees:
+    # features.append get_used_features( left subtree )
+    # features.append get_used_features( right subtree )
+    return features
