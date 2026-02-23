ce96632538b04807fd13ff5a4e7fdf9bb97e6199
Lukas Forst <lukas.forst@uni-ulm.de>
2026-02-23 18:56:50 +0100

    combine nested for loop into single loop

--- julia/neubert276.jl
+++ julia/neubert276.jl
@@ -4,10 +4,8 @@
 A = fill(0, (6, 6))
 cantorpi(x, y) = 1/2 * (x+y) * (x+y+1) + y
 
-for i = 1:6
-	for j = 1:6
-		A[i, j] = cantorpi(i-1, j-1)
-	end
+for i = 1:6, j = 1:6
+	A[i, j] = cantorpi(i-1, j-1)
 end
 
 A
